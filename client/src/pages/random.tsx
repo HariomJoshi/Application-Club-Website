0f6eb4abc0ed2bba67bfb0406ed6bd05ec38bb80
JaTin <jigyashusaini7@gmail.com>
2025-02-18 12:26:15 +0530

    adding typescript

---
+++ client/src/pages/random.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function random(): JSX.Element {
+    return (
+        <div>
+            Random
+        </div>
+    )
+}
+
+// import React from "react";
+//
+//
+// const random: React.FC = ()=> {
+//
+//     export
+//
+//     return <div>Random</div>;
+// };
+
+export default random;
